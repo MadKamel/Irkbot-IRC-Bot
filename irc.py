e94f8a8ad81b88a910701b69348ba8a2f4a5ea96
Eric Rustrum <rustrumek@gmail.com>
2020-11-18 19:31:53 +0000

    Changed tab schema to fit my style.

--- irc.py
+++ irc.py
@@ -4,26 +4,27 @@ import sys
 
 class IRC:
 
-    irc = socket.socket()
+  irc = socket.socket()
   
-    def __init__(self):  
-        self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  def __init__(self):  
+    self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def send(self, chan, msg):
-        self.irc.send("PRIVMSG " + chan + " " + msg + "n")
+  def send(self, chan, msg):
+      
+    self.irc.send("PRIVMSG " + chan + " " + msg + "n")
 
-    def connect(self, server, channel, botnick):
-        print("connecting to: " + server)
-        self.irc.connect((server, 6667))
-        print("connection complete.")
-        self.irc.send("USER " + botnick + " " + botnick + " " + botnick + " " + botnick + "n")
-        self.irc.send("NICK " + botnick + "n")
-        self.irc.send("JOIN " + channel + "n")
+  def connect(self, server, channel, botnick):
+    print("connecting to: " + server)
+    self.irc.connect((server, 6667))
+    print("connection complete.")
+    self.irc.send("USER " + botnick + " " + botnick + " " + botnick + " " + botnick + "n")
+    self.irc.send("NICK " + botnick + "n")
+    self.irc.send("JOIN " + channel + "n")
 
-    def get_text(self):
-        text = self.irc.recv(2040)
+  def get_text(self):
+    text = self.irc.recv(2040)
 
-        if text.find('PING') != -1:                      
-            self.irc.send('PONG ' + text.split()[1] + 'rn') 
+    if text.find('PING') != -1:                      
+      self.irc.send('PONG ' + text.split()[1] + 'rn') 
 
-        return text
+    return text
